9c1daae07e15d55099570a15f70474230d8c175d
Wisha Eman <wisha.eman@A006-00581.local>
2023-06-27 16:58:31 +0500

    Add Changes

--- firstDjangoProject/CRUD_app/models.py
+++ firstDjangoProject/CRUD_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Employee(models.Model):
+    emp_id = models.CharField(max_length=10)
+    emp_name = models.CharField(max_length=100)
+    emp_email = models.EmailField()
+    emp_contact = models.CharField(max_length=11)
+
+    def __str__(self):
+        return self.emp_name
