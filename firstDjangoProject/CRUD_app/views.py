308105a6507f18c96f94670f38b3d59bfea8c155
Wisha Eman <wisha.eman@A006-00581.local>
2023-06-27 17:55:51 +0500

    New Commit

--- firstDjangoProject/CRUD_app/views.py
+++ firstDjangoProject/CRUD_app/views.py
@@ -1,5 +1,5 @@
 from django.http import HttpResponse
-from django.views.generic import ListView, DetailView
+from django.views import View
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Employee
@@ -17,28 +17,27 @@ def index(request):
     return render(request, 'index.html')
 
 
-class EmployeeListView(ListView):
-    model = Employee
+# class EmployeeListView(ListView):
+#     model = Employee
+#
+#
+# class EmployeeDetailView(DetailView):
+#     model = Employee
 
 
-class EmployeeDetailView(DetailView):
-    model = Employee
+class EmployeeView(View):
+    def get(self, request, *args, **kwargs):
+        details = Employee.objects.all()
+        return render(request, './templates/employees_list.html', {'details': details})
 
 
-class EmployeeCreateView(CreateView):
-    model = Employee
-    fields = ['emp_id', 'emp_name', 'emp_email', 'emp_contact']
-    template_name = "index.html"
-    success_url = reverse_lazy('employee_list')
-
-
-class EmployeeUpdateView(UpdateView):
-    model = Employee
-    fields = ['emp_id', 'emp_name', 'emp_email', 'emp_contact']
-    template_name = 'employee_update.html'
-    success_url = reverse_lazy('employee_list')
-
-
-class EmployeeDeleteView(DeleteView):
-    model = Employee
-    success_url = reverse_lazy('employee_list')
+# class EmployeeUpdateView(UpdateView):
+#     model = Employee
+#     fields = ['emp_id', 'emp_name', 'emp_email', 'emp_contact']
+#     template_name = 'employee_update.html'
+#     success_url = reverse_lazy('employee_list')
+#
+#
+# class EmployeeDeleteView(DeleteView):
+#     model = Employee
+#     success_url = reverse_lazy('employee_list')
