0d9f7b880d7581340e32416640e70847361ad26c
Wisha Eman <wisha.eman@A006-00581.local>
2023-06-27 16:39:47 +0500

    Add Model

--- firstDjangoProject/CRUD_app/urls.py
+++ firstDjangoProject/CRUD_app/urls.py
@@ -3,9 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('employees/create/', views.employee_create, name='employee_create'),
-    path('employees/', views.employee_list, name='employee_list'),
-    path('view/<int:pk>', views.employee_view, name='employee_view'),
-    path('update/<int:pk>', views.employee_update, name='employee_update'),
-    path('delete/<int:pk>', views.employee_delete, name='employee_delete'),
+    path('employees/create/', views.EmployeeCreateView.as_view(), name='employee_create'),
+    path('employees/', views.EmployeeListView.as_view(), name='employee_list'),
+    path('view/<int:pk>', views.EmployeeDetailView.as_view(), name='employee_view'),
+    path('update/<int:pk>', views.EmployeeUpdateView.as_view(), name='employee_update'),
+    path('delete/<int:pk>', views.EmployeeDeleteView(), name='employee_delete'),
 ]
--- firstDjangoProject/CRUD_app/views.py
+++ firstDjangoProject/CRUD_app/views.py
@@ -1,4 +1,6 @@
 from django.http import HttpResponse
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Employee
 from django.forms import ModelForm
@@ -20,37 +22,28 @@ def index(request):
     return render(request, 'index.html')
 
 
-def employee_list(request):
-    details = Employee.objects.all()
-    return render(request, 'employee_list.html', {'details': details})
+class EmployeeListView(ListView):
+    model = Employee
 
 
-def employee_view(request, pk):
-    employee = get_object_or_404(Employee, pk=pk)
-    return render(request, 'employee_view.html', {'employee': employee})
+class EmployeeDetailView(DetailView):
+    model = Employee
 
 
-def employee_create(request):
-    if request.method == "POST":
-        emp_id = request.POST['emp_id']
-        emp_name = request.POST['emp_name']
-        emp_email = request.POST['emp_email']
-        emp_contact = request.POST['emp_contact']
-        obj = Employee.objects.create(emp_id=emp_id, emp_name=emp_name, emp_email=emp_email, emp_contact=emp_contact)
-        obj.save()
-        return redirect('employee_list')
+class EmployeeCreateView(CreateView):
+    model = Employee
+    fields = ['emp_id', 'emp_name', 'emp_email', 'emp_contact']
+    template_name = "index.html"
+    success_url = reverse_lazy('employee_list')
 
 
-def employee_delete(request, pk):
-    Employee.objects.filter(id=pk).delete()
-    return redirect('employee_list')
+class EmployeeUpdateView(UpdateView):
+    model = Employee
+    fields = ['emp_id', 'emp_name', 'emp_email', 'emp_contact']
+    template_name = 'employee_update.html'
+    success_url = reverse_lazy('employee_list')
 
 
-def employee_update(request, pk):
-    employee = get_object_or_404(Employee, pk=pk)
-    form = EmployeeForm(request.POST or None, instance=employee)
-    if form.is_valid():
-        form.save()
-        return redirect('employee_list')
-    return render(request, 'employee_update.html', {'form': form})
-
+class EmployeeDeleteView(DeleteView):
+    model = Employee
+    success_url = reverse_lazy('employee_list')
