73d5cbb85e1f4b652c56b5029808347b66698452
Wisha Eman <wisha.eman@A006-00581.local>
2023-07-04 13:55:51 +0500

    New Changes

--- firstDjangoProject/CRUD_app/urls.py
+++ firstDjangoProject/CRUD_app/urls.py
@@ -3,9 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    # path('employees/create/', views.EmployeeCreateView.as_view(), name='employee_create'),
-    # path('employees/', views.EmployeeListView.as_view(), name='employee_list'),
-    path('employee/get/', views.EmployeeView.as_view(), name='employee_view'),
-    # path('update/<int:pk>', views.EmployeeUpdateView.as_view(), name='employee_update'),
-    # path('delete/<int:pk>', views.EmployeeDeleteView.as_view(), name='employee_delete'),
+    path('employees/create/', views.EmployeeCreateView.as_view(), name='employee_create'),
+    path('employees/', views.EmployeeListView.as_view(), name='list'),
+    path('view/<int:pk>', views.EmployeeDetailView.as_view(), name='employee_view'),
+    path('update/<int:pk>', views.EmployeeUpdateView.as_view(), name='employee_update'),
+    path('delete/<int:pk>', views.EmployeeDeleteView.as_view(), name='employee_delete'),
 ]
